5fce1809d2507913788badeb0d8662aa6590d2bd
Joao Pedro <joaopedrobeckland@gmail.com>
2019-11-12 16:30:46 -0300

    User - Session Controller

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,19 @@
 import { Router } from 'express';
 
+import authMiddleware from './app/middlewares/auth';
+
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
 const routes = new Router();
 
 routes.get('/', (req, res) => {
   return res.json({ message: 'Hello Gympoint' });
 });
+
+routes.post('/users', UserController.store);
+routes.post('/session', SessionController.store);
+
+routes.use(authMiddleware);
 
 export default routes;
